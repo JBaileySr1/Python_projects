f0fb60e0e3489950e83622a28669fcf3ec53612d
JBaileySr1 <117868454+JBaileySr1@users.noreply.github.com>
2024-03-12 22:42:41 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,13 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}!  It makes me so excited all the time because \
+I love to {verb1}.  Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
+
+
+# print("*string*" + string variable) and print("*string*{}".format(string variable)) method are two other \
+# ways of string concantenation. Example used in code is called f strings.
