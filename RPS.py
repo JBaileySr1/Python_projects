d498608da01cd33a432e008967cb4ad01941e710
JBaileySr1 <117868454+JBaileySr1@users.noreply.github.com>
2024-03-12 23:17:05 -0400

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,23 @@
+import random
+
+def play():
+    user = input(" Choose: 'r' for rock, 'p' for paper, and 's' for scissors\n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'It\'s a tie'
+        
+    # r > s, s > p, and p > r
+    if winner(user, computer):
+        return 'You won!'
+
+    return 'You lost!'
+
+    def winner(player_one, player_two):
+        #return true if player_one wins
+        # r > s, s > p, and p > r
+        if (player_one == 'r' and player_two == 's') or (player_one == 's' and player_two == 'p') \
+           or (player_one == 'p' and player_two == 'r'):
+            return True
+
+    print(play())
